061e9c3e3094510b660a195e1e19f27daec34126
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-03-24 23:34:14 +0300

    added users indexes

--- migrations/1_add_users_table.up.sql
+++ migrations/1_add_users_table.up.sql
@@ -6,3 +6,7 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(1024) NOT NULL,
     deleted_at DATE DEFAULT NULL
 );
+
+CREATE UNIQUE INDEX login_idx ON users (login);
+CREATE INDEX email_idx ON users (email);
+CREATE INDEX phone_idx ON users (phone);
