45ff2e21613983c86aac9ecc6dcd288b8abb1151
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-03-31 17:18:51 +0300

    signup now using Preparex

--- internal/storage/postgres/signups.go
+++ internal/storage/postgres/signups.go
@@ -13,12 +13,14 @@ import (
 func (pg *Pg) SaveSignup(ctx context.Context, signup models.Signup) error {
 	const op = "postgres.Pg.SaveSignup"
 
-	row := pg.db.QueryRowxContext(
-		ctx,
-		fmt.Sprintf(`INSERT INTO signups (user_fk, service_fk, at) VALUES (%d, %d, NOW())`,
-			signup.UserId, signup.ServiceId),
-	)
-	err := row.Err()
+	stmt, err := pg.db.PreparexContext(ctx, `INSERT INTO signups (user_fk, service_fk, at) VALUES ($1, $2)`)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	row := stmt.QueryRowxContext(ctx, signup.UserId, signup.ServiceId)
+	err = row.Err()
+
 	if pgxerr.Is(err, pgerrcode.UniqueViolation) {
 		return storage.ErrSignupExists
 	}
