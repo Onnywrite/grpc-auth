072e1217e359e4672f820f5c7efe23b73262bf20
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-03-31 03:26:29 +0300

    added deleted_at to sugnups table

--- migrations/3_add_signups_table.up.sql
+++ migrations/3_add_signups_table.up.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS signups (
     service_fk BIGINT REFERENCES services(service_id) ON DELETE CASCADE,
     at TIMESTAMP NOT NULL,
     banned_at TIMESTAMP DEFAULT NULL,
+    deleted_at DATE DEFAULT NULL,
     PRIMARY KEY(user_fk, service_fk)
 );
