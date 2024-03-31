bab00652c10f85ab383a1d4d01e5511e699a3a93
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-03-31 23:46:07 +0300

    validation

--- internal/models/users.go
+++ internal/models/users.go
@@ -1,17 +1,16 @@
 package models
 
-// TODO: add validation
 type User struct {
-	Login    *string
-	Email    *string
-	Phone    *string
-	Password string
+	Login    string  `validation:"ne=me,gt=0,max=30"`
+	Email    *string `validation:"email,max=255"`
+	Phone    *string `validation:"e164"`
+	Password string  `validation:"lte=72"`
 }
 
 type SavedUser struct {
 	Id       int64 `db:"user_id"`
 	Login    string
-	Email    *string // to enable nullability
-	Phone    *string // to enable nullability
+	Email    *string
+	Phone    *string
 	Password string
 }
