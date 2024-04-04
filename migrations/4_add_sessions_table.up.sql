abae31b7d8cdf4039694acda02a07da6d0c7f96f
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-04 21:44:55 +0300

    renamed closed_at

--- migrations/4_add_sessions_table.up.sql
+++ migrations/4_add_sessions_table.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS sessions (
     browser VARCHAR(32),
     os VARCHAR(16),
     at TIMESTAMP NOT NULL DEFAULT NOW(),
-    closed_at TIMESTAMP DEFAULT NULL,
+    terminated_at TIMESTAMP DEFAULT NULL,
     UNIQUE(signup_fk, ip, browser, os)
 );
 
