2160b123d73100872c82f21ff710b28bac284a63
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-04 11:19:55 +0300

    signup models

--- internal/models/signups.go
+++ internal/models/signups.go
@@ -1,10 +1,16 @@
 package models
 
+import "time"
+
 type Signup struct {
 	UserId    int64 `db:"user_fk"`
 	ServiceId int64 `db:"service_fk"`
 }
 
 type SavedSignup struct {
-	
+	Id        int64      `db:"signup_id"`
+	UserId    int64      `db:"user_fk"`
+	ServiceId int64      `db:"service_fk"`
+	CreatedAt time.Time  `db:"at"`
+	BannedAt  *time.Time `db:"banned_at"`
 }
