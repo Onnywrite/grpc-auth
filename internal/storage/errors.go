90824e8099776cf29dd2015678379008f244fc08
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-04 20:53:55 +0300

    added ErrSessionExists

--- internal/storage/errors.go
+++ internal/storage/errors.go
@@ -4,7 +4,8 @@ import "errors"
 
 var (
 	ErrUserExists       = errors.New("user already exists")
-	ErrUserNotFound     = errors.New("user not found")
+	ErrSessionExists    = errors.New("session already exists")
 	ErrSignupExists     = errors.New("signup already exists")
+	ErrUserNotFound     = errors.New("user not found")
 	ErrNoSuchPrimaryKey = errors.New("no such primary key")
 )
