ad304415c7b7eb9fec8b3a16f864fbc286b95ec9
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-04 20:54:15 +0300

    added method to get Signup

--- internal/storage/postgres/signups.go
+++ internal/storage/postgres/signups.go
@@ -2,6 +2,8 @@ package postgres
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
 
 	sq "github.com/Masterminds/squirrel"
@@ -33,4 +35,35 @@ func (pg *Pg) SaveSignup(ctx context.Context, signup models.Signup) error {
 	}
 
 	return nil
+}
+
+func (pg *Pg) Signup(ctx context.Context, userId, serviceId int64) (*models.SavedSignup, error) {
+	const op = "postgres.Pg.Signup"
+
+	s, args, err := sq.Select("signup_id", "user_fk", "service_fk", "at", "banned_at").
+	From("signups").
+	Where(sq.Eq{"user_fk":userId, "service_fk":serviceId}).
+	PlaceholderFormat(sq.Dollar).
+	ToSql()
+
+	if err != nil {
+		return nil, fmt.Errorf("squirrel %s: %w", op, err)
+	}
+
+	stmt, err := pg.db.PreparexContext(ctx, s)
+	if err != nil {
+		return nil, fmt.Errorf("preparex %s: %w", op, err)
+	}
+
+	su := &models.SavedSignup{}
+	err = stmt.GetContext(ctx, su, args)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, storage.ErrUserNotFound
+		}
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return su, nil
 }
