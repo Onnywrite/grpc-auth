5ece1a9f5ca3c013b1afe4a9f54c96fcac10f2e2
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-04 11:21:13 +0300

    new field and new methods for storage

--- internal/services/auth/service.go
+++ internal/services/auth/service.go
@@ -6,11 +6,13 @@ import (
 	"time"
 
 	"github.com/Onnywrite/grpc-auth/internal/models"
+	"github.com/google/uuid"
 )
 
 type Storage interface {
 	SaveUser(ctx context.Context, user *models.User) (*models.SavedUser, error)
 	SaveSignup(ctx context.Context, signup models.Signup) error
+	SaveSession(ctx context.Context, session *models.Session) (*models.SavedSession, error)
 
 	UserById(ctx context.Context, id int64) (*models.SavedUser, error)
 	UserByLogin(ctx context.Context, login string) (*models.SavedUser, error)
@@ -18,15 +20,17 @@ type Storage interface {
 	UserByPhone(ctx context.Context, phone string) (*models.SavedUser, error)
 
 	Signup(ctx context.Context, userId, serviceId int64) (*models.SavedSignup, error)
+
+	TerminateSession(ctx context.Context, uuid uuid.UUID) error
 }
 
 type AuthServiceImpl struct {
-	log      *slog.Logger
-	db       Storage
-	tokenTTL time.Duration
+	log                       *slog.Logger
+	db                        Storage
+	tokenTTL, refreshTokenTTL time.Duration
 }
 
-func New(logger *slog.Logger, db Storage, tokenTTL time.Duration) *AuthServiceImpl {
+func New(logger *slog.Logger, db Storage, tokenTTL, refreshTokenTTL time.Duration) *AuthServiceImpl {
 	return &AuthServiceImpl{
 		log:      logger,
 		db:       db,
