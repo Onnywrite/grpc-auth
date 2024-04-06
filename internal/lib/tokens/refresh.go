ed8e899a3b241e6b341a6c70abc41b8395cf2b9b
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-07 01:36:55 +0300

    lib to work with tokens

---
+++ internal/lib/tokens/refresh.go
@@ -0,0 +1,57 @@
+package tokens
+
+import (
+	"os"
+	"time"
+
+	"github.com/Onnywrite/grpc-auth/internal/models"
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+)
+
+func Refresh(token *models.RefreshToken) (string, error) {
+	return New(jwt.MapClaims{
+		"session_uuid": token.SessionUUID.String(),
+		"exp":          token.Exp,
+	})
+}
+
+func ParseRefresh(tkn string) (*models.RefreshToken, error) {
+	token, err := jwt.Parse(tkn, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, ErrUnexpectedSigningMethod
+		}
+
+		return []byte(os.Getenv(Env)), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		exp := claims["exp"].(float64)
+
+		if float64(time.Now().Unix()) > exp {
+			return nil, ErrTokenExpired
+		}
+
+		sessionUUIDStr, ok := claims["session_uuid"].(string)
+		if !ok {
+			return nil, ErrInvalidData
+		}
+		sessionUUID, err := uuid.Parse(sessionUUIDStr)
+		if err != nil {
+			return nil, ErrInvalidData
+		}
+
+		token := &models.RefreshToken{
+			SessionUUID: sessionUUID,
+			Exp:         int64(exp),
+		}
+
+		return token, nil
+	}
+
+	return nil, err
+}
