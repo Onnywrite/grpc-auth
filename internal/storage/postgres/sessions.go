80957d92f5618f52a8c612fcc1c4667653d29f0e
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-07 22:22:02 +0300

    using new signature + getting rid of squirrel

--- internal/storage/postgres/sessions.go
+++ internal/storage/postgres/sessions.go
@@ -5,14 +5,12 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"net/netip"
 	"time"
 
 	sq "github.com/Masterminds/squirrel"
 	"github.com/Onnywrite/grpc-auth/internal/lib/pgxerr"
 	"github.com/Onnywrite/grpc-auth/internal/models"
 	storage "github.com/Onnywrite/grpc-auth/internal/storage/common"
-	"github.com/google/uuid"
 	"github.com/jackc/pgerrcode"
 )
 
@@ -26,12 +24,12 @@ func (pg *Pg) SaveSession(ctx context.Context, session *models.Session) (*models
 			FROM signups
 			WHERE user_fk = $1 AND service_fk = $2
 		), $3, $4, $5)
-		RETURNING session_uuid, signup_fk, ip, browser, os, created_at`)
+		RETURNING session_uuid, signup_fk, ip, browser, os, at`)
 	if err != nil {
 		return nil, fmt.Errorf("preparex %s: %w", op, err)
 	}
 
-	row := stmt.QueryRowxContext(ctx, session.UserId, session.ServiceId, session.IP, session.Browser, session.OS)
+	row := stmt.QueryRowxContext(ctx, session.UserId, session.ServiceId, session.Info.Ip, session.Info.Browser, session.Info.OS)
 
 	err = row.Err()
 	if pgxerr.Is(err, pgerrcode.UniqueViolation) {
@@ -42,38 +40,20 @@ func (pg *Pg) SaveSession(ctx context.Context, session *models.Session) (*models
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	type scannedSession struct {
-		UUID         string `db:"session_uuid"`
-		SignupId     int64  `db:"signup_fk"`
-		IP           netip.Addr
-		Browser      string
-		OS           string
-		CreatedAt    time.Time `db:"at"`
-		TerminatedAt *time.Time
-	}
-
-	scanned := &scannedSession{}
-	err = row.StructScan(scanned)
+	ss := &models.SavedSession{}
+	err = row.StructScan(ss)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	return &models.SavedSession{
-		UUID:         uuid.MustParse(scanned.UUID),
-		SignupId:     scanned.SignupId,
-		IP:           scanned.IP,
-		Browser:      scanned.Browser,
-		OS:           scanned.OS,
-		CreatedAt:    scanned.CreatedAt,
-		TerminatedAt: scanned.TerminatedAt,
-	}, nil
+	return ss, nil
 }
 
-func (pg *Pg) DeleteSession(ctx context.Context, uuid uuid.UUID) error {
+func (pg *Pg) DeleteSession(ctx context.Context, uuid string) error {
 	const op = "postgres.Pg.DeleteSession"
 
 	s, args, err := sq.Delete("sessions").
-		Where(sq.Eq{"session_uuid": uuid.String()}).
+		Where(sq.Eq{"session_uuid": uuid}).
 		PlaceholderFormat(sq.Dollar).
 		ToSql()
 	if err != nil {
@@ -98,11 +78,11 @@ func (pg *Pg) DeleteSession(ctx context.Context, uuid uuid.UUID) error {
 	return nil
 }
 
-func (pg *Pg) Session(ctx context.Context, uuid uuid.UUID) (*models.SavedSession, error) {
-	const op = "postgres.Pg.DeleteSession"
+func (pg *Pg) SessionById(ctx context.Context, uuid string) (*models.SavedSession, error) {
+	const op = "postgres.Pg.SessionById"
 
 	s, args, err := sq.Select("sessions").
-		Where(sq.Eq{"session_uuid": uuid.String()}).
+		Where(sq.Eq{"session_uuid": uuid}).
 		PlaceholderFormat(sq.Dollar).
 		ToSql()
 	if err != nil {
@@ -127,12 +107,41 @@ func (pg *Pg) Session(ctx context.Context, uuid uuid.UUID) (*models.SavedSession
 	return session, nil
 }
 
-func (pg *Pg) TerminateSession(ctx context.Context, uuid uuid.UUID) error {
-	const op = "postgres.Pg.DeleteSession"
+func (pg *Pg) Session(ctx context.Context, session *models.Session) (*models.SavedSession, error) {
+	const op = "postgres.Pg.Session"
+
+	stmt, err := pg.db.PreparexContext(ctx, `
+	SELECT *
+	FROM sessions
+	WHERE signup_fk IN(
+		SELECT signup_id
+		FROM signups
+		WHERE user_fk = $1 AND service_fk = $2
+	) AND browser = $3 AND ip = $4 AND os = $5
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("preparex %s: %w", op, err)
+	}
+
+	saved := &models.SavedSession{}
+	err = stmt.GetContext(ctx, saved, session.UserId, session.ServiceId, session.Info.Browser, session.Info.Ip, session.Info.OS)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, storage.ErrEmptyResult
+	}
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return saved, nil
+}
+
+func (pg *Pg) TerminateSession(ctx context.Context, uuid string) error {
+	const op = "postgres.Pg.TerminateSession"
 
 	s, args, err := sq.Update("sessions").
 		Set("terminated_at", time.Now()).
-		Where(sq.And{sq.Eq{"session_uuid": uuid.String()}, sq.Eq{"terminated_at": nil}}).
+		Where(sq.And{sq.Eq{"session_uuid": uuid}, sq.Eq{"terminated_at": nil}}).
 		PlaceholderFormat(sq.Dollar).
 		ToSql()
 	if err != nil {
