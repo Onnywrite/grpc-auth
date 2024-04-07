fb45fa4d03ad5857f5828f73dea6c7ded8b871b0
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-07 22:22:36 +0300

    new AuthService interface signature

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -11,8 +11,8 @@ import (
 
 type AuthService interface {
 	Register(ctx context.Context, optLogin, optEmail, optPhone *string, password string) error
-	Signup(ctx context.Context, key, password string, serviceId int64) error
-	Login(ctx context.Context, key, password string, serviceId int64, browser, os, ip string) (*models.Tokens, error)
+	Signup(ctx context.Context, identifier models.UserIdentifier, serviceId int64) error
+	Login(ctx context.Context, identifier models.UserIdentifier, sessionInfo models.SessionInfo, serviceId int64) (*models.Tokens, error)
 	Logout(ctx context.Context, refresh string) error
 }
 
