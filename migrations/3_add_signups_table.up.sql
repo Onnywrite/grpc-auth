5aac0f479803a2c84e712f5124a21249cde674e2
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-10 00:27:28 +0300

    fixed bug with multiple signups

--- migrations/3_add_signups_table.up.sql
+++ migrations/3_add_signups_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS signups (
     signup_id BIGSERIAL PRIMARY KEY,
-    user_fk BIGINT REFERENCES users(user_id) ON DELETE CASCADE,
-    service_fk BIGINT REFERENCES services(service_id) ON DELETE CASCADE,
+    user_fk BIGINT REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
+    service_fk BIGINT REFERENCES services(service_id) ON DELETE CASCADE NOT NULL,
     at TIMESTAMP NOT NULL DEFAULT NOW(),
     banned_at TIMESTAMP DEFAULT NULL,
     deleted_at DATE DEFAULT NULL
