cbeeb2ee6d69d0167308e8a052a77bc987a1b1b7
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-10 00:28:08 +0300

    added default data

---
+++ migrations/5_insert_defaults.down.sql
@@ -0,0 +1,6 @@
+DELETE FROM users
+WHERE user_id = 0;
+DELETE FROM services
+WHERE service_id = 0;
+DELETE FROM signups
+WHERE signup_id = 0;
---
+++ migrations/5_insert_defaults.up.sql
@@ -0,0 +1,4 @@
+-- password: rootroot
+INSERT INTO users (user_id, login, password) VALUES (0, 'root', '$2a$10$f9PsJKZKQeE.0JEz8qoRtuTUVQZB1nnSP7SeLx0KpYmj8w1JZb0sC');
+INSERT INTO services (service_id, name, owner_fk) VALUES (0, 'ssonny', 0);
+INSERT INTO signups (signup_id, user_fk, service_fk) VALUES (0, 0, 0);
