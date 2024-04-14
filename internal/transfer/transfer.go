ce85158c0457d8acaff86c785f999608668cc5ef
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-14 17:38:26 +0300

    new method - Signout

--- internal/transfer/transfer.go
+++ internal/transfer/transfer.go
@@ -9,13 +9,14 @@ import (
 
 type AuthService interface {
 	Register(ctx context.Context, user *models.User, info models.SessionInfo) (*gen.IdTokens, error)
-	// Recover(ctx context.Context, user *models.User, info models.SessionInfo) (*gen.IdTokens, error)
 	Login(ctx context.Context, user *models.User, info models.SessionInfo) (*gen.IdTokens, error)
+	// Recover(ctx context.Context, user *models.User, info models.SessionInfo) (*gen.IdTokens, error)
 	// Logout(ctx context.Context, idToken string) error
 
 	Signup(ctx context.Context, idToken string, serviceId int64, info models.SessionInfo) (*gen.AppTokens, error)
 	// RecoverSignup(ctx context.Context, idToken string, serviceId int64, info models.SessionInfo) (*gen.AppTokens, error)
 	Signin(ctx context.Context, idToken string, serviceId int64, info models.SessionInfo) (*gen.AppTokens, error)
+	Signout(ctx context.Context, refresh string) error
 	Resignin(ctx context.Context, refresh string) error
 	Check(ctx context.Context, access string) error
 	// Unsign(ctx context.Context, access string) error
