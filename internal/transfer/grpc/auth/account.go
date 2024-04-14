6a251da7cd88c0d3782290cc495a8e5d057d9c77
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-14 18:36:12 +0300

    alpha-implemented RPCs

--- internal/transfer/grpc/auth/account.go
+++ internal/transfer/grpc/auth/account.go
@@ -4,9 +4,9 @@ import (
 	"context"
 
 	"github.com/Onnywrite/grpc-auth/gen"
+	"github.com/Onnywrite/grpc-auth/internal/models"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 //var (
@@ -14,8 +14,26 @@ import (
 //invalidLoginOrPassword = "invalid login or password"
 //)
 
-func (a authServer) Register(c context.Context, u *gen.InRequest) (*gen.IdTokens, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Logout not implemented")
+func (a authServer) Register(c context.Context, r *gen.InRequest) (*gen.IdTokens, error) {
+	user := &models.User{
+		Login:    r.Credentials.Login,
+		Email:    r.Credentials.Email,
+		Phone:    r.Credentials.Phone,
+		Password: r.Credentials.Password,
+	}
+
+	info := models.SessionInfo{
+		Browser: r.SessionInfo.Browser,
+		Ip:      r.SessionInfo.Ip,
+		OS:      r.SessionInfo.Os,
+	}
+
+	resp, err := a.service.Register(c, user, info)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "error: %s", err.Error())
+	}
+
+	return resp, nil
 }
 
 func (authServer) Recover(context.Context, *gen.InRequest) (*gen.IdTokens, error) {
@@ -23,9 +41,23 @@ func (authServer) Recover(context.Context, *gen.InRequest) (*gen.IdTokens, error
 }
 
 func (a authServer) Login(c context.Context, r *gen.InRequest) (*gen.IdTokens, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Logout not implemented")
-}
+	user := &models.User{
+		Login:    r.Credentials.Login,
+		Email:    r.Credentials.Email,
+		Phone:    r.Credentials.Phone,
+		Password: r.Credentials.Password,
+	}
 
-func (authServer) Logout(context.Context, *gen.IdToken) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Logout not implemented")
+	info := models.SessionInfo{
+		Browser: r.SessionInfo.Browser,
+		Ip:      r.SessionInfo.Ip,
+		OS:      r.SessionInfo.Os,
+	}
+
+	resp, err := a.service.Login(c, user, info)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "error: %s", err.Error())
+	}
+
+	return resp, nil
 }
