2ad4621ac7b34991737eea3025b96f0806a0d40b
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-17 01:13:01 +0300

    FIXED: sql syntax error

--- migrations/4_add_sessions_table.up.sql
+++ migrations/4_add_sessions_table.up.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS sessions (
     session_uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_fk BIGINT ON DELETE CASCADE NOT NULL,
-    service_fk BIGINT ON DELETE CASCADE NOT NULL,
+    user_fk BIGINT NOT NULL,
+    service_fk BIGINT NOT NULL,
     ip CIDR NOT NULL,
     browser VARCHAR(32),
     os VARCHAR(16),
     at TIMESTAMP NOT NULL DEFAULT NOW(),
     terminated_at TIMESTAMP DEFAULT NULL,
     UNIQUE(user_fk, service_fk, ip, browser, os),
-    FOREIGN KEY (user_fk, service_fk) REFERENCES signups(user_fk, service_fk)
+    FOREIGN KEY (user_fk, service_fk) REFERENCES signups(user_fk, service_fk) ON DELETE CASCADE
 );
 
 CREATE INDEX sessions_fk_idx ON sessions (user_fk, service_fk);
