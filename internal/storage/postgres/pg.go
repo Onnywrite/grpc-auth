be73fe5fe637e0f7370088ed17b7fe68a0ae4b9d
Onnywrite <vlad.mokeev.061611@gmail.com>
2024-04-25 00:54:04 +0300

    replace pgxerr package with new funcs for postgres

--- internal/storage/postgres/pg.go
+++ internal/storage/postgres/pg.go
@@ -1,8 +1,14 @@
 package postgres
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 
+	"github.com/Onnywrite/grpc-auth/internal/lib/ero"
+	storage "github.com/Onnywrite/grpc-auth/internal/storage/common"
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
 )
@@ -26,4 +32,37 @@ func NewPg(conn string) (*Pg, error) {
 
 func (pg *Pg) Disconnect() error {
 	return pg.db.Close()
+}
+
+var (
+	sqlerrToErr = map[string]*ero.StorageError{
+		pgerrcode.UniqueViolation:     storage.ErrUniqueConstraint,
+		pgerrcode.ForeignKeyViolation: storage.ErrFKConstraint,
+		sql.ErrNoRows.Error():         storage.ErrEmptyResult,
+	}
+)
+
+func pgerr(anyerr error, op string) error {
+	pgErr := &pgconn.PgError{}
+	var strerr string
+	if errors.As(anyerr, &pgErr) {
+		strerr = pgErr.Code
+	} else {
+		strerr = anyerr.Error()
+	}
+
+	if err, ok := sqlerrToErr[strerr]; ok {
+		err.WithMethod(op)
+		return err
+	}
+
+	return ero.NewStorage(anyerr.Error()).WithMethod(op)
+}
+
+func preperr(anyerr error, op string) error {
+	return ero.NewStorage(anyerr.Error()).WithMethod(op).SetCode(ero.CodeStorageInternal)
+}
+
+func scanerr(anyerr error, op string) error {
+	return ero.NewStorage(anyerr.Error()).WithMethod(op).SetCode(ero.CodeStorageInternal)
 }
