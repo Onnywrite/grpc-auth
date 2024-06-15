194e53dc812e7a5990bad4f33d3c794d445219d4
Onnywrite <vlad.mokeev161106@gmail.com>
2024-06-15 20:17:48 +0300

    changed __From ero conversion

--- internal/lib/ero/internal.go
+++ internal/lib/ero/internal.go
@@ -2,19 +2,38 @@ package ero
 
 type Internal struct {
 	*Basic
-	Function string
+	CallStack []string
 }
 
 func NewInternal(function string, errors ...string) *Internal {
 	return &Internal{
-		Basic:    New(errors...),
-		Function: function,
+		Basic:     New(errors...),
+		CallStack: []string{function},
 	}
 }
 
-func InternalFrom(function string, basic *Basic) *Internal {
-	return &Internal{
-		Basic:    basic,
-		Function: function,
+func InternalFrom(function string, err Error) *Internal {
+	switch terr := err.(type) {
+	case *Internal:
+		terr.CallStack = append(terr.CallStack, function)
+		return terr
+	case *Server:
+		terr.CallStack = append(terr.CallStack, function)
+		return &Internal{
+			Basic:     terr.Basic,
+			CallStack: terr.CallStack,
+		}
+	case *Client:
+		return &Internal{
+			Basic:     terr.Basic,
+			CallStack: []string{function},
+		}
+	case *Basic:
+		return &Internal{
+			Basic:     terr,
+			CallStack: []string{function},
+		}
+	default:
+		panic("invalid error type")
 	}
 }
--- internal/lib/ero/server.go
+++ internal/lib/ero/server.go
@@ -2,19 +2,38 @@ package ero
 
 type Server struct {
 	*Basic
-	Function string
+	CallStack []string
 }
 
 func NewServer(function string, errors ...string) *Server {
 	return &Server{
-		Basic:    New(errors...),
-		Function: function,
+		Basic:     New(errors...),
+		CallStack: []string{function},
 	}
 }
 
-func ServerFrom(function string, basic *Basic) *Server {
-	return &Server{
-		Basic:    basic,
-		Function: function,
+func ServerFrom(function string, err Error) *Server {
+	switch terr := err.(type) {
+	case *Internal:
+		terr.CallStack = append(terr.CallStack, function)
+		return &Server{
+			Basic:     terr.Basic,
+			CallStack: terr.CallStack,
+		}
+	case *Server:
+		terr.CallStack = append(terr.CallStack, function)
+		return terr
+	case *Client:
+		return &Server{
+			Basic:     terr.Basic,
+			CallStack: []string{function},
+		}
+	case *Basic:
+		return &Server{
+			Basic:     terr,
+			CallStack: []string{function},
+		}
+	default:
+		panic("invalid error type")
 	}
 }
