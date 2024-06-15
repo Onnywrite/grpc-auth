349146962024b8d52782da17a414a9c980660989
Onnywrite <vlad.mokeev161106@gmail.com>
2024-06-15 17:24:41 +0300

    moved to new ero, inited 'nickname' tag

--- internal/lib/validation/validation.go
+++ internal/lib/validation/validation.go
@@ -2,47 +2,60 @@ package validation
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
 	"github.com/Onnywrite/grpc-auth/internal/lib/ero"
 	"github.com/go-playground/validator/v10"
 )
 
+const (
+	ErrContextDone = "context done"
+)
+
 type ValidateFn func(*validator.Validate, any) error
 
-func ValidateWith(ctx context.Context, validate ValidateFn, structs ...any) error {
+func ValidateWith(ctx context.Context, validate ValidateFn, structs ...any) ero.Error {
+	const op = "validation.ValidateWith"
+
 	wg := sync.WaitGroup{}
 	wg.Add(len(structs))
-	validationErrors := ero.NewValidation()
+	errors := ero.New()
 
 	v := validator.New()
+	v.RegisterValidation("nickname", validateNickname)
+
 	for i := range structs {
-		go func() {
+		go func(ii int) {
 			defer wg.Done()
 			select {
 			case <-ctx.Done():
-				validationErrors.SetCode(ero.CodeValidationTimeout)
+				errors.Add(ErrContextDone)
+				// TODO: add logging
 				return
 			default:
-				if err := validate(v, structs[i]); err != nil {
+				if err := validate(v, structs[ii]); err != nil {
 					errs := err.(validator.ValidationErrors)
 					for _, e := range errs {
-						validationErrors.AddField(e.StructField(), e.Tag(), e.Value())
+						errors.Add(fmt.Sprintf("field %s has invalid value %s", e.StructField(), fmt.Sprint(e.Value())))
 					}
 				}
 			}
-		}()
+		}(i)
 	}
 
 	wg.Wait()
-
-	if len(validationErrors.Errors) > 0 || validationErrors.Code != ero.CodeValidation {
-		return validationErrors
+	switch {
+	case errors.Has(ErrContextDone):
+		return ero.ServerFrom(op, errors)
+	case len(errors.Errors) > 0:
+		return ero.ClientFrom(errors)
+	default:
+		return nil
 	}
-	return nil
 }
 
-func Validate(ctx context.Context, structs ...any) error {
+func Validate(ctx context.Context, structs ...any) ero.Error {
 	return ValidateWith(ctx, func(v *validator.Validate, s any) error {
 		return v.StructCtx(ctx, s)
 	}, structs...)
