91e5d3cf5116f6e4a108a9aa185640203d264ebf
Onnywrite <vlad.mokeev161106@gmail.com>
2024-06-22 21:43:40 +0300

    moved wrapper to auth package

--- internal/services/auth/service.go
+++ internal/services/auth/service.go
@@ -1,22 +1,40 @@
 package auth
 
 import (
+	"context"
 	"log/slog"
 	"time"
 
-	"github.com/Onnywrite/grpc-auth/internal/services/auth/wrap"
+	"github.com/Onnywrite/grpc-auth/internal/lib/ero"
+	"github.com/Onnywrite/grpc-auth/internal/models"
 )
+
+type Storage interface {
+	SaveUser(ctx context.Context, user *models.User) (*models.SavedUser, ero.Error)
+	UserById(ctx context.Context, id int64) (*models.SavedUser, ero.Error)
+	UserByNickname(ctx context.Context, login string) (*models.SavedUser, ero.Error)
+	UserByEmail(ctx context.Context, email string) (*models.SavedUser, ero.Error)
+	UserByPhone(ctx context.Context, phone string) (*models.SavedUser, ero.Error)
+
+	SaveSignup(ctx context.Context, signup models.Signup) (*models.SavedSignup, ero.Error)
+	SignupByServiceAndUser(ctx context.Context, serviceId, userId int64) (*models.SavedSignup, ero.Error)
+
+	SaveSession(ctx context.Context, session *models.Session) (*models.SavedSession, ero.Error)
+	SessionByUuid(ctx context.Context, uuid string) (*models.SavedSession, ero.Error)
+	SessionByInfo(ctx context.Context, serviceId, userId int64, info models.SessionInfo) (*models.SavedSession, ero.Error)
+	DeleteSession(ctx context.Context, uuid string) ero.Error
+}
 
 type AuthService struct {
 	log                                            *slog.Logger
-	db                                             *wrap.Wrapper
+	db                                             Storage
 	tokenTTL, refreshTokenTTL, superAccessTokenTTL time.Duration
 }
 
-func New(logger *slog.Logger, db wrap.Storage, tokenTTL, refreshTokenTTL, superAccessTokenTTL time.Duration) *AuthService {
+func New(logger *slog.Logger, db Storage, tokenTTL, refreshTokenTTL, superAccessTokenTTL time.Duration) *AuthService {
 	return &AuthService{
 		log:                 logger,
-		db:                  wrap.New(logger, db),
+		db:                  NewWrapper(logger, db),
 		tokenTTL:            tokenTTL,
 		superAccessTokenTTL: superAccessTokenTTL,
 		refreshTokenTTL:     refreshTokenTTL,
--- internal/services/auth/wrapper.go
+++ internal/services/auth/wrapper.go
@@ -1,4 +1,4 @@
-package wrap
+package auth
 
 import (
 	"context"
@@ -6,32 +6,15 @@ import (
 
 	"github.com/Onnywrite/grpc-auth/internal/lib/ero"
 	"github.com/Onnywrite/grpc-auth/internal/models"
-	auth "github.com/Onnywrite/grpc-auth/internal/services/auth/common"
 	storage "github.com/Onnywrite/grpc-auth/internal/storage/common"
 )
-
-type Storage interface {
-	SaveUser(ctx context.Context, user *models.User) (*models.SavedUser, ero.Error)
-	UserById(ctx context.Context, id int64) (*models.SavedUser, ero.Error)
-	UserByLogin(ctx context.Context, login string) (*models.SavedUser, ero.Error)
-	UserByEmail(ctx context.Context, email string) (*models.SavedUser, ero.Error)
-	UserByPhone(ctx context.Context, phone string) (*models.SavedUser, ero.Error)
-
-	SaveSignup(ctx context.Context, signup models.Signup) (*models.SavedSignup, ero.Error)
-	SignupByServiceAndUser(ctx context.Context, serviceId, userId int64) (*models.SavedSignup, ero.Error)
-
-	SaveSession(ctx context.Context, session *models.Session) (*models.SavedSession, ero.Error)
-	SessionByUuid(ctx context.Context, uuid string) (*models.SavedSession, ero.Error)
-	SessionByInfo(ctx context.Context, serviceId, userId int64, info models.SessionInfo) (*models.SavedSession, ero.Error)
-	DeleteSession(ctx context.Context, uuid string) ero.Error
-}
 
 type Wrapper struct {
 	log *slog.Logger
 	Storage
 }
 
-func New(logger *slog.Logger, db Storage) *Wrapper {
+func NewWrapper(logger *slog.Logger, db Storage) *Wrapper {
 	return &Wrapper{
 		log:     logger,
 		Storage: db,
@@ -51,10 +34,10 @@ func (w *Wrapper) SaveSession(ctx context.Context, session *models.Session) (*mo
 	switch {
 	case ero.Has(err, storage.ErrFKConstraint):
 		log.Error("user has never signed up")
-		return nil, ero.NewClient(auth.ErrSignupNotExists)
+		return nil, ero.NewClient(ErrSignupNotExists)
 	case ero.Has(err, storage.ErrUniqueConstraint):
 		log.Error("session already exists")
-		return nil, ero.NewClient(auth.ErrSessionAlreadyOpened)
+		return nil, ero.NewClient(ErrSessionAlreadyOpened)
 	case err != nil:
 		log.Error("error saving session", slog.String("error", err.Error()))
 		return nil, ero.InternalFrom(op, err)
@@ -99,7 +82,7 @@ func (w *Wrapper) session(ctx context.Context, get getSessionFn, keys ...any) (*
 	switch {
 	case ero.Has(err, storage.ErrEmptyResult):
 		log.Error("session not found")
-		return nil, ero.NewClient(auth.ErrSessionNotExists)
+		return nil, ero.NewClient(ErrSessionNotExists)
 	case err != nil:
 		log.Error("error getting session", slog.String("error", err.Error()))
 		return nil, ero.InternalFrom(op, err)
@@ -125,7 +108,7 @@ func (w *Wrapper) SaveSignup(ctx context.Context, signup models.Signup) (*models
 	switch {
 	case ero.Has(err, storage.ErrFKConstraint):
 		log.Error("service does not exist")
-		return nil, ero.NewClient(auth.ErrServiceNotExists)
+		return nil, ero.NewClient(ErrServiceNotExists)
 	case ero.Has(err, storage.ErrUniqueConstraint):
 		log.Error("user already signed up, checking details")
 		su, err = w.Storage.SignupByServiceAndUser(ctx, signup.ServiceId, signup.UserId)
@@ -135,13 +118,13 @@ func (w *Wrapper) SaveSignup(ctx context.Context, signup models.Signup) (*models
 		}
 		if su.IsDeleted() {
 			log.Error("signed out")
-			return nil, ero.NewClient(auth.ErrSignedOut)
+			return nil, ero.NewClient(ErrSignedOut)
 		}
 		if su.IsBanned() {
 			log.Error("signup banned")
-			return nil, ero.NewClient(auth.ErrSignupBanned)
+			return nil, ero.NewClient(ErrSignupBanned)
 		}
-		return nil, ero.NewClient(auth.ErrAlreadySignedUp)
+		return nil, ero.NewClient(ErrAlreadySignedUp)
 	case err != nil:
 		log.Error("saving error", slog.String("error", err.Error()))
 		return nil, ero.InternalFrom(op, err)
@@ -180,7 +163,7 @@ func (w *Wrapper) signup(ctx context.Context, get getSignupFn, keys ...any) (*mo
 	switch {
 	case ero.Has(err, storage.ErrEmptyResult):
 		log.Error("signup not found")
-		return nil, ero.NewClient(auth.ErrSignupNotExists)
+		return nil, ero.NewClient(ErrSignupNotExists)
 	case err != nil:
 		log.Error("cannot get signup", slog.String("error", err.Error()))
 		return nil, ero.InternalFrom(op, err)
@@ -188,12 +171,12 @@ func (w *Wrapper) signup(ctx context.Context, get getSignupFn, keys ...any) (*mo
 
 	if su.IsDeleted() {
 		log.Error("signed out")
-		return su, ero.NewClient(auth.ErrSignedOut)
+		return su, ero.NewClient(ErrSignedOut)
 	}
 
 	if su.IsBanned() {
 		log.Error("signup banned")
-		return su, ero.NewClient(auth.ErrSignupBanned)
+		return su, ero.NewClient(ErrSignupBanned)
 	}
 
 	log.Info("got signup")
@@ -213,13 +196,13 @@ func (w *Wrapper) SaveUser(ctx context.Context, user *models.User) (*models.Save
 	u, err := w.Storage.SaveUser(ctx, user)
 	switch {
 	case ero.Has(err, storage.ErrUniqueConstraint):
-		u, err = w.UserByLogin(ctx, user.Login)
-		if err.Has(auth.ErrUserDeleted) {
+		u, err = w.UserByNickname(ctx, user.Nickname)
+		if err.Has(ErrUserDeleted) {
 			log.Error("user deleted", slog.Int64("id", u.Id), slog.String("error", err.Error()))
 			return nil, err
 		}
 		log.Error("user already exists", slog.Int64("id", u.Id))
-		return nil, ero.NewClient(auth.ErrUserAlreadyRegistered)
+		return nil, ero.NewClient(ErrUserAlreadyRegistered)
 	case err != nil:
 		log.Error("cannot save user", slog.String("error", err.Error()))
 		return nil, ero.InternalFrom(op, err)
@@ -235,10 +218,10 @@ func (w *Wrapper) SaveUser(ctx context.Context, user *models.User) (*models.Save
 //	ErrInvalidCredentials
 //	ErrUserDeleted
 //	Internal
-func (w *Wrapper) UserByLogin(ctx context.Context, login string) (*models.SavedUser, ero.Error) {
+func (w *Wrapper) UserByNickname(ctx context.Context, nickname string) (*models.SavedUser, ero.Error) {
 	return w.user(ctx, func(ctx context.Context, key any) (*models.SavedUser, ero.Error) {
-		return w.Storage.UserByLogin(ctx, key.(string))
-	}, login, "login")
+		return w.Storage.UserByNickname(ctx, key.(string))
+	}, nickname, "nickname")
 }
 
 // Throws:
@@ -289,7 +272,7 @@ func (w *Wrapper) user(ctx context.Context, get getUserFn, key any, keyType stri
 	switch {
 	case ero.Has(err, storage.ErrEmptyResult):
 		log.Error("no user found")
-		return nil, ero.NewClient(auth.ErrInvalidCredentials)
+		return nil, ero.NewClient(ErrInvalidCredentials)
 	case err != nil:
 		log.Error("getting error", slog.String("error", err.Error()))
 		return nil, ero.InternalFrom(op, err)
@@ -297,7 +280,7 @@ func (w *Wrapper) user(ctx context.Context, get getUserFn, key any, keyType stri
 
 	if saved.IsDeleted() {
 		log.Error("user deleted")
-		return saved, ero.NewClient(auth.ErrUserDeleted)
+		return saved, ero.NewClient(ErrUserDeleted)
 	}
 
 	return saved, nil
